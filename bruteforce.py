ebd579227c5ee69980409ddd6854e732915407c8
Kevin Niland <kevinpniland@gmail.com>
2020-12-08 12:33:06 +0000

    Current project

--- bruteforce.py
+++ bruteforce.py
@@ -1,38 +1,57 @@
 import argparse
-import sys
-# import pikepdf
 import re
-from tqdm import tqdm
-
-# Issues
-# ======
-# How to deal with a password such as 00001? 000234? Python doesn't consider 00001 a valid int. When converting the string "00001"
-# to an int, it will simply be 1. This can pose a problem when the password is of length 6, for example - program won't get a hit
-# on the correct password as it will remove any leading 0's (currently)
-# Based on research, look into zfill - adds zero to start of string until it reaches specified length
+from pikepdf import Pdf
 
 # Logic
-def crack_password(digits, file):
-    i = 0
+# def crack_password(digits, filename, pattern):
+def crack_password(digits, filename):
+    i = 1
+    i_max = 9999999999999999999 % (10**digits)
+    while i < i_max + 1:
+        password_num = str(i).zfill(digits)
 
-    while i < sys.maxsize:
-        password = str(i).zfill(digits)
-
+        # if pattern is None or pattern.match(password_num):
         try:
-            Pdf.open(filname_or_stream = file, password = password)
-            print("Password: " + password)
+            Pdf.open(filename_or_stream = filename, password = password_num)
+            print("SUCCESS: Password - " + password_num)
 
             return True
         except:
+            # Incorrect password, keep trying
+            print(i)
             pass
         
         i += 1
     
     return False
 
+# Command line arguments
 parser = argparse.ArgumentParser(description="PDF Numeric Password Cracker")
 parser.add_argument('filename', help="Full path of the PDF file")
+parser.add_argument('-min', '--min-digits', help="Minimum digits in password", type=int, default="1")
+parser.add_argument('-max', '--max-digits', help="Maximum digits in password", type=int, default="-1")
+parser.add_argument('-r', '--matching-regex', help="Skip passwords not matching regex", type=str, default=None)
 args = parser.parse_args()
 
+min_digits = args.min_digits
+max_digits = args.max_digits
+pattern = None
+
+if max_digits < min_digits:
+    max_digits = min_digits
+
+# if args.matching_regex is not None:
+#     pattern = re.compile(args.matching_regex)
+#     print('Skipping passwords not matching:', args.matching_regex)
+
+digits = min_digits
+hacked = False
 print("Attempting to crack password...")
-crack_password(6, args.filename)
+
+while not hacked and digits <= max_digits:
+    # hacked = crack_password(digits, args.filename, pattern)
+    hacked = crack_password(digits, args.filename)
+    digits += 1
+
+if not hacked:
+    print("FAIL: Unable to crack password for this file")
